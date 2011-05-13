e8d87aff1c5871b5fc48ed5180b595e7ceb79995
valera <vvvval@mail333.com>
2011-05-13 19:18:03 -0400

    Added multiple attachments feature.

--- emailScript.py
+++ emailScript.py
@@ -35,7 +35,7 @@ def getText():
     return text
 
 
-def sendMails(h, text, *attachmentFilePaths):
+def sendMails(h, text):
     gotError = False
     p = re.compile('^.*@.*$')
     gmailUser = raw_input("What's your full email or user name (default is @movein.to)? ")
@@ -52,7 +52,7 @@ def sendMails(h, text, *attachmentFilePaths):
     subject = raw_input("Please enter the subject line: ")
     greeting = raw_input("Please enter your greeting (e.g. Dear, Hi,...): ")
     fromStr = raw_input("Please enter your name: ")
-    attachmentFilePath = raw_input("Please directory with files to attach: ")
+    attachmentDirPath = raw_input("Please directory with files to attach: ")
 
     mailServer = smtplib.SMTP('smtp.gmail.com', 587)
     mailServer.ehlo()
@@ -69,7 +69,7 @@ def sendMails(h, text, *attachmentFilePaths):
         msg['To'] = recipient
         msg['From'] = fromStr
         msg.attach(MIMEText(personal_text))
-	msg.attach(getAttachment(attachmentFilePath))
+	getAttachments(msg,attachmentDirPath)
         try:
             mailServer.sendmail(gmailUser, recipient, msg.as_string())
             print('Sent email to %s at %s' % (t[NAME],recipient))
@@ -84,29 +84,38 @@ def sendMails(h, text, *attachmentFilePaths):
     if gotError:
         print 'Some error(s) occured. Please see the text generated above!\n'
 
-def getAttachment(attachmentFilePath):
-    contentType, encoding = mimetypes.guess_type(attachmentFilePath)
-    if contentType is None or encoding is not None:
-        contentType = 'application/octet-stream'
-    mainType, subType = contentType.split('/', 1)
+def getAttachments(msg,attachmentDirPath):
+    #default to current directory
+    if not attachmentDirPath:
+        attachmentDirPath = '.'
 
-    fp = open(attachmentFilePath, 'rb')
+    for filename in os.listdir(attachmentDirPath):
+        path = os.path.join(attachmentDirPath, filename)
+        if not os.path.isfile(path):
+            continue
 
-    if mainType == 'text':
-        attachment = MIMEText(fp.read())
-    elif mainType == 'image':
-        attachment = MIMEImage(fp.read(),_subType=subType)
-    elif mainType == 'audio':
-        attachment = MIMEAudio(fp.read(),_subType=subType)
-    else:
-        attachment = MIMEBase(mainType, subType)
-        attachment.set_payload(fp.read())
-        encode_base64(attachment)
+        contentType, encoding = mimetypes.guess_type(path)
+        if contentType is None or encoding is not None:
+            contentType = 'application/octet-stream'
+        mainType, subType = contentType.split('/', 1)
 
-    fp.close()
-    attachment.add_header('Content-Disposition', 'attachment', 
-                          filename=os.path.basename(attachmentFilePath))
-    return attachment
+        fp = open(path, 'rb')
+
+        if mainType == 'text':
+            attachment = MIMEText(fp.read())
+        elif mainType == 'image':
+            attachment = MIMEImage(fp.read(),_subType=subType)
+        elif mainType == 'audio':
+            attachment = MIMEAudio(fp.read(),_subType=subType)
+        else:
+            attachment = MIMEBase(mainType, subType)
+            attachment.set_payload(fp.read())
+            encode_base64(attachment)
+            fp.close()
+
+        attachment.add_header('Content-Disposition', 'attachment', 
+                                  filename=os.path.basename(path))
+        msg.attach(attachment)
 
 if __name__ == "__main__":
     print "Please make sure there is 'namesEmails.csv' and 'text.txt' fps in the script folder before proceeding."
